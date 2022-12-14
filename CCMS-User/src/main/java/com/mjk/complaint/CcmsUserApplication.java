de578a0667d08b64c209072a275d738e23edf6a1
RUmmesalma <114227710+RUmmesalma@users.noreply.github.com>
2022-12-14 10:47:28 +0530

    Add files via upload

---
+++ CCMS-User/src/main/java/com/mjk/complaint/CcmsUserApplication.java
@@ -0,0 +1,13 @@
+package com.mjk.complaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcmsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcmsUserApplication.class, args);
+	}
+
+}
